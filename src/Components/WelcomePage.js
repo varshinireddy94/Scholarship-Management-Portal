93cecbf4fc1ca7d0b48fe42c5774ce65ecc6637c
Varshini Reddy <151116777+varshinireddy94@users.noreply.github.com>
2025-06-07 23:30:47 +0530

    Add files via upload

---
+++ src/Components/WelcomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Card } from '@material-ui/core';
+export default class WelcomePage extends React.Component{
+
+render(){
+    return(
+        <Card style={{margin:10,minWidth:300, display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"center",textAlign:"center"}}>
+       
+            <h1>Welocome to Scholarship Portal</h1>
+            <img src="https://www.midwestscc.org/blog2/wp-content/uploads/2016/01/scholarships.png"/>
+
+            </Card>
+    )
+}
+
+}
